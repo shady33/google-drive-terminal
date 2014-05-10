9523aea3f95c1f98dbbeda8a12aeacdf332ea111
shady33 <bhatialaksh3@gmail.com>
2014-05-11 03:46:47 +0530

    Added oAuth2.0 saving credentials

--- drive.py
+++ drive.py
@@ -2,28 +2,71 @@
 
 import httplib2
 import pprint
+import sys
 
 from apiclient.discovery import build
 from apiclient.http import MediaFileUpload
 from oauth2client.client import OAuth2WebServerFlow
 from apiclient import errors
+from oauth2client.file import Storage
+
+def insert_file(service, title, description, parent_id, mime_type, filename):
+  
+  media_body = MediaFileUpload(filename, mimetype=mime_type, resumable=False)
+  body = {
+    'title': title,
+    'description': description,
+    'mimeType': mime_type
+  }
+  # Set the parent folder.
+  if parent_id:
+    body['parents'] = [{'id': parent_id}]
+
+  try:
+    file = service.files().insert(
+        body=body,
+        media_body=media_body).execute()
+
+    return file
+  except errors.HttpError, error:
+    print 'An error occured: %s' % error
+    return None
+
+def update_file(service, file_id, new_title, new_description, new_mime_type,
+                new_filename, new_revision):
+
+  try:
+    # First retrieve the file from the API.
+    file = service.files().get(fileId=file_id).execute()
+
+    # File's new metadata.
+    file['title'] = new_title
+    file['description'] = new_description
+    file['mimeType'] = new_mime_type
+
+    # File's new content.
+    media_body = MediaFileUpload(
+        new_filename, mimetype=new_mime_type, resumable=True)
+
+    # Send the request to the API.
+    updated_file = service.files().update(
+        fileId=file_id,
+        body=file,
+        newRevision=new_revision,
+        media_body=media_body).execute()
+    return updated_file
+  except errors.HttpError, error:
+    print 'An error occurred: %s' % error
+    return None
 
 def write_file(file_name,content):
+
 	file=open(file_name,'w')
 	file.write(content)
 	file.close()
 
-
 def download_file(service, drive_file,file_name):
-  """Download a file's content.
 
-  Args:
-    service: Drive API service instance.
-    drive_file: Drive File instance.
-
-  Returns:
-    File's content if successful, None otherwise.
-  """
   download_url = drive_file.get('downloadUrl')
   print download_url
   if download_url:
@@ -39,6 +82,14 @@ def download_file(service, drive_file,file_name):
     # The file doesn't have any content stored on Drive.
     return None
 
+
+
+if len(sys.argv) > 1:
+    print "Argument Given:"+sys.argv[1]
+else:
+    print "Please enter a argument"
+    sys.exit()
+
 f=open('credentials.txt','r')
 credential=f.read().splitlines()
 # Copy your credentials from the console
@@ -50,13 +101,18 @@ OAUTH_SCOPE = 'https://www.googleapis.com/auth/drive'
 
 # Redirect URI for installed apps
 REDIRECT_URI = 'urn:ietf:wg:oauth:2.0:oob'
-
-# Run through the OAuth flow and retrieve credentials
-flow = OAuth2WebServerFlow(CLIENT_ID, CLIENT_SECRET, OAUTH_SCOPE, REDIRECT_URI)
-authorize_url = flow.step1_get_authorize_url()
-print 'Go to the following link in your browser: ' + authorize_url
-code = raw_input('Enter verification code: ').strip()
-credentials = flow.step2_exchange(code)
+storage = Storage('a_credentials_file')
+try:
+  credentials = storage.get()
+except:
+  print "error"
+  # Run through the OAuth flow and retrieve credentials
+  flow = OAuth2WebServerFlow(CLIENT_ID, CLIENT_SECRET, OAUTH_SCOPE, REDIRECT_URI)
+  authorize_url = flow.step1_get_authorize_url()
+  print 'Go to the following link in your browser: ' + authorize_url
+  code = raw_input('Enter verification code: ').strip()
+  credentials = flow.step2_exchange(code)
+  storage.put(credentials)
 
 # Create an httplib2.Http object and authorize it with our credentials
 http = httplib2.Http()
@@ -64,24 +120,14 @@ http = credentials.authorize(http)
 
 drive_service = build('drive', 'v2', http=http)
 
-# Insert a file
-#media_body = MediaFileUpload(FILENAME, mimetype='text/plain', resumable=True)
-body = {
-'title': 'My document',
-'description': 'A test document',
-'mimeType': 'text/plain'
-}
-
-#file = drive_service.files().insert(body=body, media_body=media_body).execute()
-#pprint.pprint(file)
-
-result = []
-page_token = None
-while True:
-	try:
+if sys.argv[1]== "pull":
+  result = []
+  page_token = None
+  while True:
+	 try:
 	  param = {}
 	  #if page_token:
-	  param['q'] = credential[2] + " in parents"
+	  param['q'] = "'"+credential[2] + "' in parents"
 	  files = drive_service.files().list(**param).execute()
 	  #files = drive_service.files().list(fileId='').execute()
 	  for filex in files['items']:
@@ -91,7 +137,12 @@ while True:
 	  #page_token = files.get('nextPageToken')
 	  #if not page_token:
 	  break
-	except errors.HttpError, error:
+	 except errors.HttpError, error:
 	  print 'An error occurred: %s' % error
 	  break
-print result
+  print result
+elif sys.argv[1]=="push":
+  update_file(drive_service, credential[2], "new_title", "new_description", "text/xml",
+                "filename", "new_revision")
+  #insert_file(drive_service, "title", "description", "0B2UfUTlp455beWRHb3doMWZCeGc", "text/xml", "filename")
+

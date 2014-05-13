8a698a5fa03b7c5206de92a1a3cd1bc4498c8c94
shady33 <bhatialaksh3@gmail.com>
2014-05-13 15:43:47 +0530

    Init,Push,Pull,Add,Origin commands added

--- drive.py
+++ drive.py
@@ -3,12 +3,13 @@
 import httplib2
 import pprint
 import sys
-
+import os
 from apiclient.discovery import build
 from apiclient.http import MediaFileUpload
 from oauth2client.client import OAuth2WebServerFlow
 from apiclient import errors
 from oauth2client.file import Storage
+import os.path
 
 def insert_file(service, title, description, parent_id, mime_type, filename):
   
@@ -59,9 +60,9 @@ def update_file(service, file_id, new_title, new_description, new_mime_type,
     print 'An error occurred: %s' % error
     return None
 
-def write_file(file_name,content):
+def write_file(file_name,content,mode):
 
-	file=open(file_name,'w')
+	file=open(file_name,mode)
 	file.write(content)
 	file.close()
 
@@ -74,7 +75,7 @@ def download_file(service, drive_file,file_name):
     print resp.status
     if resp.status == 200:
       print 'Status: %s' % resp
-      write_file(file_name,content)
+      write_file(file_name,content,"w")
     else:
       print 'An error occurred: %s' % resp
       return None
@@ -82,7 +83,33 @@ def download_file(service, drive_file,file_name):
     # The file doesn't have any content stored on Drive.
     return None
 
+def authenticate():
+  f=open('.drive/credentials.txt','r')
+  credential=f.read().splitlines()
+  f.close()
+  CLIENT_ID = credential[0]
+  CLIENT_SECRET = credential[1]
 
+  OAUTH_SCOPE = 'https://www.googleapis.com/auth/drive'
+  REDIRECT_URI = 'urn:ietf:wg:oauth:2.0:oob'
+  storage = Storage('.drive/a_credentials_file')
+  credentials = storage.get()
+  
+  if credentials == None:
+    flow = OAuth2WebServerFlow(CLIENT_ID, CLIENT_SECRET, OAUTH_SCOPE, REDIRECT_URI)
+    authorize_url = flow.step1_get_authorize_url()
+    print 'Go to the following link in your browser: ' + authorize_url
+    code = raw_input('Enter verification code: ').strip()
+    credentials = flow.step2_exchange(code)
+    storage.put(credentials)
+  return credentials
+
+def connect():
+  credentials=authenticate()
+  http = httplib2.Http()
+  http = credentials.authorize(http)
+  drive_service = build('drive', 'v2', http=http)
+  return drive_service
 
 if len(sys.argv) > 1:
     print "Argument Given:"+sys.argv[1]
@@ -90,58 +117,72 @@ else:
     print "Please enter a argument"
     sys.exit()
 
-f=open('credentials.txt','r')
-credential=f.read().splitlines()
-# Copy your credentials from the console
-CLIENT_ID = credential[0]
-CLIENT_SECRET = credential[1]
+if sys.argv[1]=="init":
+  directory=os.getcwd()+"/.drive"
+  if not os.path.exists(directory):
+    os.makedirs(directory)
+    print "Initialized the directory"
 
-# Check https://developers.google.com/drive/scopes for all available scopes
-OAUTH_SCOPE = 'https://www.googleapis.com/auth/drive'
+elif sys.argv[1]=="origin":
+  if len(sys.argv) > 2:
+    url=sys.argv[2].split("/")[4]
+    write_file(".drive/credentials.txt",url,"a")
+    authenticate()
+  else:
+    print "Enter the url"
 
-# Redirect URI for installed apps
-REDIRECT_URI = 'urn:ietf:wg:oauth:2.0:oob'
-storage = Storage('a_credentials_file')
-try:
-  credentials = storage.get()
-except:
-  print "error"
-  # Run through the OAuth flow and retrieve credentials
-  flow = OAuth2WebServerFlow(CLIENT_ID, CLIENT_SECRET, OAUTH_SCOPE, REDIRECT_URI)
-  authorize_url = flow.step1_get_authorize_url()
-  print 'Go to the following link in your browser: ' + authorize_url
-  code = raw_input('Enter verification code: ').strip()
-  credentials = flow.step2_exchange(code)
-  storage.put(credentials)
+elif sys.argv[1]=="add":
+  if len(sys.argv) > 2:
+    fname=os.getcwd()+"/.drive/fnames"
+    if not os.path.isfile(fname):
+      write_file(fname,sys.argv[2]+"\n",'w')
+    else:
+      f=open('.drive/fnames','r')
+      pathnames=f.read().splitlines()
+      try:
+        if not filter(lambda x: sys.argv[2] in x, pathnames)[0] == sys.argv[2]:
+          write_file(fname,sys.argv[2]+"\n",'a')
+        else:
+          print "Already in file"
+      except:
+        write_file(fname,sys.argv[2]+"\n",'a')
+  else:
+    print "Add a filename"  
 
-# Create an httplib2.Http object and authorize it with our credentials
-http = httplib2.Http()
-http = credentials.authorize(http)
 
-drive_service = build('drive', 'v2', http=http)
-
-if sys.argv[1]== "pull":
-  result = []
-  page_token = None
-  while True:
-	 try:
-	  param = {}
-	  #if page_token:
-	  param['q'] = "'"+credential[2] + "' in parents"
-	  files = drive_service.files().list(**param).execute()
-	  #files = drive_service.files().list(fileId='').execute()
-	  for filex in files['items']:
-	  	print filex['title']
-	  	download_file(drive_service,filex,filex['title'])
-	  #result.extend(files['items'])
-	  #page_token = files.get('nextPageToken')
-	  #if not page_token:
+elif sys.argv[1]== "pull":
+  drive_service=connect()
+  f=open('.drive/credentials.txt','r')
+  credential=f.read().splitlines()
+  f.close()
+  try:
+    param = {}
+    param['q'] = "'"+credential[2] + "' in parents"
+    files = drive_service.files().list(**param).execute()
+    if len(sys.argv) > 2:
+      if sys.argv[2]=="all":
+        for filex in files['items']:
+          download_file(drive_service,filex,filex['title'])
+    else:
+      for filex in files['items']:
+        print filex['title']
+        f=open('.drive/fnames','r')
+        pathnames=f.read().splitlines()
+        try:
+          if filter(lambda x: filex['title'] in x, pathnames)[0] == filex['title']:
+            download_file(drive_service,filex,filex['title'])
+        except:
+          print ""
+  	  
 	  break
-	 except errors.HttpError, error:
-	  print 'An error occurred: %s' % error
-	  break
-  print result
+  except errors.HttpError, error:
+    print 'An error occurred: %s' % error
+	   
 elif sys.argv[1]=="push":
+  drive_service=connect()
+  f=open('.drive/credentials.txt','r')
+  credential=f.read().splitlines()
+  f.close()
   update_file(drive_service, credential[2], "new_title", "new_description", "text/xml",
                 "filename", "new_revision")
   #insert_file(drive_service, "title", "description", "0B2UfUTlp455beWRHb3doMWZCeGc", "text/xml", "filename")
